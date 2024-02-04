b8c3f7ebb9588e4bbd78891f3795176c053e7489
codewithdruvo <druvokd23@gmail.com>
2024-02-04 21:49:04 +0600

    added crypto modal

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
+import { SearchContextProvider } from "../context/SearchContext";
 
 const MainLayout = ({ children }) => (
-  <>
+  <SearchContextProvider>
     <Navbar />
     <div className="container py-10">{children}</div>
-  </>
+  </SearchContextProvider>
 );
 
 export default MainLayout;
