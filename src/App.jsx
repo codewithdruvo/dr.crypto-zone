c196292569e3933bc32a13b4a0dc3b58c19e474a
codewithdruvo <druvokd23@gmail.com>
2024-02-04 21:08:45 +0600

    implements page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
+import Home from "./pages/Home";
+import "./static/styles/main.css";
 
-const App = () => {
-  return <MainLayout />;
-};
+function App() {
+  return (
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h2>404 not found</h2>} />
+      </Routes>
+    </MainLayout>
+  );
+}
 
 export default App;
