5ecf41f616b86274864cda59035c250e164fa4d4
codewithdruvo <druvokd23@gmail.com>
2024-02-04 21:10:49 +0600

    create search page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Home from "./pages/Home";
 import "./static/styles/main.css";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <MainLayout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/search/" element={<Search />} />
         <Route path="*" element={<h2>404 not found</h2>} />
       </Routes>
     </MainLayout>
